d5d7ef60634dc23939360b1d9daf18af550f12c8
Waris Hayat <warishayat69@gmail.com>
2023-11-26 14:18:26 +0000

    day 4 work uploaded

---
+++ day4.py
@@ -0,0 +1,37 @@
+#in 4/70 day of challenge i will cover the bsic of operaotrs there are diffrent type o opreator>
+#like addition (+),subtraction(-),multiplication(*),division(/),modulus(%),exponential (**) and floor division(//)
+
+n1=int(input("Enter the first number:- "))
+n2=int(input("Enter the second number:- "))
+print("The Addition of these two number is: ",n1+n2)
+print("The Subtraction of these two number is: ",n1-n2)
+print("The Division of these two number is: ",n1/n2)
+print("The Multiplication of these two number is: ",n1*n2)
+print("The Modulus  of these two number is: ",n1%n2)
+print("The exponential of these two number is: ",n1**n2)
+print("The floor division of these two number is: ",n1//n2)
+
+#Assignment operator
+i=5
+j=10
+print(" ",i,j)
+j*=i
+print(" ",i,j)
+
+#comparision operator 
+n11=4
+n22=5
+if n1==n22:
+    print("Number are same:-")
+else:
+    print("Number are not same:-")
+
+n13=4
+n25=5
+is_flag=True
+if n13!=n25:
+    True
+else:
+    False
+
+##logical operators AND (All shoud be true) or (atleast one shoukd be true) not (reverse the result if result is true it will gave us false)
