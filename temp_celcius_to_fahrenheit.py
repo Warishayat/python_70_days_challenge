da7eced8777df436d6c00d573b3e5953316efea0
Waris Hayat <warishayat69@gmail.com>
2023-11-26 14:27:21 +0000

    Day 4 task 2

---
+++ temp_celcius_to_fahrenheit.py
@@ -0,0 +1,5 @@
+#taking temprature in celcius
+celcius=float(input("Enter the temprature in celcius:-"))
+#calculating the temprature in fahrenheit.
+fahrenheit=(celcius*9/5)+32
+print("The temprature in fahrenheit is",fahrenheit)
