9402a3f331e88dcbc15938a6d36300cf61b77b0a
Waris Hayat <warishayat69@gmail.com>
2023-11-26 14:22:59 +0000

    day 4 task

---
+++ Odd_problem.py
@@ -0,0 +1,8 @@
+#Ask user's to enter the number and tell them where its even or odd. its the part of if else loop.
+
+user_num=int(input("Enter the Number to check Even/Odd:-) "))
+
+if user_num % 2 == 0:
+    print("The number you had enterd is Even :-) ")
+else:
+    print("The number you had enterd is Odd :-) ")
