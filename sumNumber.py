5a588dfa806f512c7cee2c0d4ad38a4879ea5286
Waris Hayat <warishayat69@gmail.com>
2023-11-26 14:29:29 +0000

    day 4 task 3

---
+++ sumNumber.py
@@ -0,0 +1,5 @@
+#print the sum of the given two number:
+num1=int(input("Enter the first number:-"))
+num2=int(input("Enter the second number:-"))
+sum=num1+num2
+print(f"The sum of the given two number is={sum}")
