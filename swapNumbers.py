251afca1f478580fdea1b382d67c280a2a25f835
Waris Hayat <warishayat69@gmail.com>
2023-11-26 14:26:30 +0000

    Day 4 task

---
+++ swapNumbers.py
@@ -0,0 +1,7 @@
+num1=int(input("Ener the First Number:-"))
+num2=int(input("Ener the First Number:-"))
+print(f"the value before swapping swaping is num1 has {num1} and num2 has {num2}")
+num3=num1
+num1=num2
+num2=num3
+print(f"the value after swaping is num1 has {num1} and num2 has {num2}")
