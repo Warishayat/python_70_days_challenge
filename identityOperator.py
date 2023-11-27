f8e303f18b70f36121eaa01f58f019c7c9abd4d2
Waris Hayat <warishayat69@gmail.com>
2023-11-27 18:24:08 +0000

    Identity Operator example 2

---
+++ identityOperator.py
@@ -0,0 +1,7 @@
+#Create two variables, x and y, and assign them the same integer value.
+#Use the identity operator to check if x is the same object as y. Print the result.
+
+x=123
+y=123
+
+print(x is  y)
