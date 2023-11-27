dbabece1a337d397db22824a9b0f014bcd4492eb
Waris Hayat <warishayat69@gmail.com>
2023-11-27 18:13:20 +0000

    Logical Operator example 3

---
+++ problemLogicalOperatorII.py
@@ -0,0 +1,18 @@
+#Question2Define three variables: a, b, and c.
+#Write a Python expression that evaluates to True if a is equal to b or c is greater than b.
+
+var_a=int(input("Enter the variable a:- "))
+var_b=int(input("Enter the variable b:- "))
+var_c=int(input("Enter the variable c:- "))
+
+is_flag=True
+
+if var_a==var_b or var_c > var_b:
+    is_flag
+    print(is_flag)
+else:
+    is_flag=False
+    print(is_flag)
+    
+
+
