1d037d29afc077ddc1cf89b4c9ab00d9cf317ecb
Waris Hayat <warishayat69@gmail.com>
2023-11-29 16:11:51 +0000

    +IVE OR -IVE number check example

---
+++ Negativeproblem.py
@@ -0,0 +1,8 @@
+#ask user to enter te number and tell them where this number is positive or negative:
+#ITS ALSO the part of 6/70 day challenge or if else loop part.
+num=int(input("Enter a number to check +ive or -ive :-"))
+
+if num>=0:
+    print("Succesfully you enter the +ive number:")
+else:
+    print("Alas! you enter the -ive number:")
