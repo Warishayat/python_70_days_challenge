fc60519cc0eb08dafbc9138df7c6e78db8eae8df
Waris Hayat <warishayat69@gmail.com>
2023-11-29 16:22:49 +0000

    Control Statment like if and else

--- day6.py
+++ day6.py
@@ -1,9 +1,9 @@
-# today the day 6/70 of challenge and today we will try to cover the basic of loops and control statments 
+#today we will try to cover the basic of loops and control statments 
 #more over i will also try to solve the question of conditions and loops and i wil  try topush all that thing to github.
-#it willhep us to control the flow of the program.i take the disicion on the basis of the control statments 
+#it will hep us to control the flow of the program.i take the disicion on the basis of the control statments 
 # if and else mean if condition is true or false.
 
-rain=int(input("its raining or not:- "))
+rain=int(input("its raining or not: "))
 
 if rain==True:                  # 1 in case of true 0 in case of false:
     print("Take the Umbrella with you")
