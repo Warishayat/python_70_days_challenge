d4ec1a9d451c3974cc49509900dd9ade94191cbb
Waris Hayat <warishayat69@gmail.com>
2023-12-01 17:40:16 +0000

    Nested example3

---
+++ NestedLoop_example3.py
@@ -0,0 +1,21 @@
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+for i in range(1,6,1): # 1to5
+    for n in range(1,i+1): #1 to i
+        print(i,end ="")
+    print()
+
+print()
+# 1
+# 12
+# 123
+# 1234
+# 12345
+for i in range(1,6,1): # 1to5
+    for n in range(1,i+1): #1 to i
+        print("*",end ="")
+    print()
