6134362a1dda8e5baaf16224e28d55da4d5f618e
Waris Hayat <warishayat69@gmail.com>
2023-12-08 17:19:12 +0000

    Function work

---
+++ day15.py
@@ -0,0 +1,16 @@
+#in day 15 i will cover the function.its used for code sepration.
+def average(a=10,b=20):
+
+    p=(a+b)/2
+    print("The average is=",p)
+print()
+
+def sum(a,b):
+    print("The sum of two values are=",a+b)
+
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the second number: "))
+
+average(n1,n2)
+sum(n1,n2)
+
