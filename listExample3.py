6b1cb4448cfc4cbd3ce172b6ebd4cddfa7336746
Waris Hayat <warishayat69@gmail.com>
2023-12-11 18:49:26 +0000

    List example4

---
+++ listExample3.py
@@ -0,0 +1,19 @@
+
+# Create a list of squares for numbers from 1 to 5.
+
+l1=[n**2 for n in range(1,5)]
+print(l1)
+
+
+# Create a list of the first letter of each word in a sentence
+fruit=["mango", "pineapple", "avacado","watermelon","Dragon","Grapes"]
+for i in fruit:
+    print(i[0])
+
+
+# Create a list of even numbers from 1 to 10..
+
+l=[l for l in range(1,10) if l%2==0]
+print(l)
+
+
