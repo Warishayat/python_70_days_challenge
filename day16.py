e148bb13b34c2833283c6a69878a9ca8a307b33f
Waris Hayat <warishayat69@gmail.com>
2023-12-11 18:30:09 +0000

    Diffrent type of functio arguments like positional,default,keyward,variable length arguments

---
+++ day16.py
@@ -0,0 +1,57 @@
+#we pass arguments though the function and i will explore that today.
+
+
+# def avg(a,b):
+#     print('The avg is',(a+b)/2)
+
+# avg(30,4) 
+
+
+# 1:these arguments are called default arguments:
+#  2: keyword argument where argument order doesnot be change.
+# 4: variable length arguments
+#  3: v required arguments which are compuslory
+
+
+#2:
+def avg(a,b):
+    print('The avg is',(a+b)/2)
+
+avg(b=10,a=16)  #keyword arguments
+
+# def average(*numbers):
+#     sum=0
+#     for i in numbers:
+#         sum= sum+i
+#         print('Aaverage is', sum / len( numbers ))
+
+# average(5,6)
+
+# def name(fname="Hayat",mname='Abbasi',lname="Waris"):
+#     print(f"My name is {fname} and middle name is {mname} and last name is {lname}")
+
+
+# name(lname="Saaaab")
+
+
+                        #variable length arguments:#
+
+def average(*numbers):
+    sum=0
+    for i in numbers:
+        sum=sum+i
+    return sum/len(numbers)
+    print(type(numbers))
+
+c=average(4,6,2)
+print(c)
+
+
+# def values (**name):
+#     print(type(name))
+#     print("",name["name"],name['mname'],name['lname'])
+
+# values(name="kashan",mname="khan",lname="jameel")
+
+
+
