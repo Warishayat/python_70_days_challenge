0e0ccf22d5a530931b1b7feee5497d32b09af58a
Waris Hayat <warishayat69@gmail.com>
2023-12-11 18:48:44 +0000

    List  example3

---
+++ ListExamplePractise1.py
@@ -0,0 +1,17 @@
+# Creating Lists:
+
+# Create an empty list.
+# Create a list with integers from 1 to 5.
+# Create a list containing strings of your favorite fruits.
+
+l=[] #empty list
+print(l)
+
+#2
+
+l=[1,2,3,4,5]
+print(l)
+
+#3
+fruit=["mango", "pineapple", "avacado","watermelon","Dragon","Grapes"]
+print(fruit)
