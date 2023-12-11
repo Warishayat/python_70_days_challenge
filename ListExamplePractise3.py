a95f6d25987c4c1a56af5fa536df1f73839b69c4
Waris Hayat <warishayat69@gmail.com>
2023-12-11 18:47:29 +0000

    List examples1

---
+++ ListExamplePractise3.py
@@ -0,0 +1,14 @@
+# # Concatenate two lists.
+l1=[1,2,3,4,5]
+l2=[6,7,8,9,10]
+concate_list=l1+l2
+print(concate_list)
+
+# Repeat a list three times
+l1=[1,2,3,4,5]
+print(l1*3, end =" ")
+print()
+# Find the length of a list.
+l2=[6,7,8,9,10]
+print(len(l2))
+print(l2)
