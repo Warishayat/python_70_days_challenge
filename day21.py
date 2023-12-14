bb04719528295b69175b5893a9dcecdc81631595
Waris Hayat <warishayat69@gmail.com>
2023-12-14 16:47:45 +0000

    Formatting string concept

---
+++ day21.py
@@ -0,0 +1,38 @@
+#Formatiing string
+
+
+name="Waris Hayat Abbasi"
+University="National university o modren langues"
+cgpa="A"
+roolnum="SP-21368"
+Semester="4"
+Section="A"
+Father_name="Umer Hayat Abbasi"
+
+#now i will make formatting of string
+
+print(f"my name is {name}. My father name is {Father_name}.I study in {University}. My roolnum is {roolnum}.My section is {Section}.I'm in {Semester} semester.My cgpe is {cgpa} and my section is{Section}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Basic String Formatting:
+# Create a program that takes a person's name, age, and city as input and prints a message using string formatting. For example: "Hello, [Name]! You are [Age] years old and live in [City]."
+Name="waris"
+city="Islamabad"
+age="22"
+
+print(f"My name is {name}. I live is {city}. My age is {age}. ")
