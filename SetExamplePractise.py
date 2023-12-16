880acb902eaab57813d84bb058e49c0d76c82ab6
Waris Hayat <warishayat69@gmail.com>
2023-12-16 16:38:34 +0000

    Set Example4

---
+++ SetExamplePractise.py
@@ -0,0 +1,10 @@
+# You are given two sets of integers, set_A and set_B. 
+# Write a Python fu(nction to find the symmetric difference of the 
+# two sets, i.e., the set of elements that are in either 
+# of the sets, but not in both. Implement the algorithm
+
+
+setA={1,2,3,4,5,6,7,8,9}
+setB={11,12,13,14,15,6,7,8,9}
+
+print("The symmetric element are:",setA.symmetric_difference(setB))
