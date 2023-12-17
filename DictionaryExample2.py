fccd0b07ec3651202f33c42415212423891e6120
Waris Hayat <warishayat69@gmail.com>
2023-12-17 16:45:31 +0000

    How to remove a  value from dictionary

---
+++ DictionaryExample2.py
@@ -0,0 +1,14 @@
+# Adding and Removing Elements:
+# Create an empty dictionary and add three key-value pairs to it.
+# Remove one key-value pair from the dictionary and 
+# print the updated dictionary.
+
+
+dic={
+
+}
+dic.update({'name': "waris Hayat Abbasi","age":22, "Status":"student","Cnic":3740458865757})
+
+#how to remove some key and their value fro the dictionary.
+dic.pop('age')
+print(dic)
