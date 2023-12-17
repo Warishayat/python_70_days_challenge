f77c9056937f552e482174963b1032532d0e8d3d
Waris Hayat <warishayat69@gmail.com>
2023-12-17 16:44:20 +0000

    How to get some value out from dictionary

---
+++ DictionaryExample5.py
@@ -0,0 +1,17 @@
+#nested library
+
+
+dic={
+    'java':{1:"maria" ,'cnic':36173649191,'father_name':'ajmal','Subject':'software'},
+    'oops':{2:"ayesa" ,'cnic':363784924591,'father_name':'akhil','Subject':'bs-cs'},
+    'dsa':{3:"neha" ,'cnic':36137744591,'father_name':'norraj','Subject':'software_eng'},
+    'python':{2:"simran" ,'cnic':3613664591,'father_name':'durani','Subject':'software_eng'},
+    'php':{4:"aleeza" ,'cnic':363435591,'father_name':'zzamin','Subject':'software_eng'},
+    'asp.net':{5:"iqra" ,'cnic':36128764591,'father_name':'wahaj','Subject':'software_eng'},
+    'c++':{6:"zainab" ,'cnic':36467924591,'father_name':'zulli','Subject':'software_eng'},
+    'c#':{7:"adan" ,'cnic':36121884891,'father_name':'zeeshu','Subject':'software_eng'},
+    'java':{8:"norah" ,'cnic':36125754591,'father_name':'tabi','Subject':'software_eng'},
+    'ruby':{9:"katrina" ,'cnic':36121927771,'father_name':'tashfeen','Subject':'software_eng'},
+    }   
+
+print(dic['c++']['father_name'])
