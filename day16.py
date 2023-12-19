7afd040f5206641cd18267b68a7881def6be6446
Waris Hayat <warishayat69@gmail.com>
2023-12-19 15:12:57 +0000

    Functions and type of functions

--- day16.py
+++ day16.py
@@ -1,10 +1,16 @@
 #we pass arguments though the function and i will explore that today.
 
+#positional arguments
+def avg(a,b):
+    print('The avg is',(a+b)/2)
+avg(4,5) 
 
-# def avg(a,b):
-#     print('The avg is',(a+b)/2)
 
-# avg(30,4) 
+
+#default arguments
+def avg(a,b=6):
+    print('The avg is',(a+b)/2)
+avg(30) 
 
 
 # 1:these arguments are called default arguments:
@@ -19,39 +25,38 @@ def avg(a,b):
 
 avg(b=10,a=16)  #keyword arguments
 
-# def average(*numbers):
-#     sum=0
-#     for i in numbers:
-#         sum= sum+i
-#         print('Aaverage is', sum / len( numbers ))
-
-# average(5,6)
-
-# def name(fname="Hayat",mname='Abbasi',lname="Waris"):
-#     print(f"My name is {fname} and middle name is {mname} and last name is {lname}")
-
-
-# name(lname="Saaaab")
-
-
-                        #variable length arguments:#
-
+#variable length arguments.
 def average(*numbers):
     sum=0
     for i in numbers:
+        sum= sum+i
+        print('Aaverage is', sum / len( numbers ))
+average(5,6)
+
+
+# **keywargs arguments
+def name(fname="Hayat",mname='Abbasi',lname="Waris"):
+    print(f"My name is {fname} and middle name is {mname} and last name is {lname}")
+name(lname="Saaaab")
+
+
+#variable length arguments:#
+def average(*numbers):      
+    sum=0
+    for i in numbers:
         sum=sum+i
-    return sum/len(numbers)
+    return sum/len(numbers)  #return value back to the calling function
     print(type(numbers))
 
 c=average(4,6,2)
 print(c)
 
 
-# def values (**name):
-#     print(type(name))
-#     print("",name["name"],name['mname'],name['lname'])
+def values (**name):
+    print(type(name))
+    print("",name["name"],name['mname'],name['lname'])
 
-# values(name="kashan",mname="khan",lname="jameel")
+values(name="kashan",mname="khan",lname="jameel")
 
 
 
