0bbde483936fd29f30d0ea0b4382ddce80f9d8a5
Waris Hayat <warishayat69@gmail.com>
2023-12-19 15:11:59 +0000

    Shoet hand if else

---
+++ day26.py
@@ -0,0 +1,19 @@
+#shorthand if else:
+#our else always work with for loop
+
+for i in range(4):
+    print(i)
+else:
+    print("The else is working with loop")
+print()
+
+
+
+for i in range(10):
+
+    if i==5:
+        break
+    print(i)
+
+else:
+    print("yEs else always work with loop ")
