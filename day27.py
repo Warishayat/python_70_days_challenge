b54592d032ecedf49e10a670041813283fed5002
Waris Hayat <warishayat69@gmail.com>
2023-12-20 17:25:27 +0000

    Final eyword

---
+++ day27.py
@@ -0,0 +1,33 @@
+#today we will cover the final keyword 
+#try and excepthandle the error where final keyword use for cleanup.mean a thing 
+#that we want to execute in every conditoin.
+
+
+#example1
+
+try:
+ l=[1,2,3,4,5,6,7,8]
+ n=int(input("Enter the index"))
+ print(l[n])
+except:
+ print("Here we have index error")
+finally:
+    print("This line of code will be ececuted in every condtion")
+
+#if we do the dame thing with function
+def fun():
+    try:
+        l=[1,2,3,4,5,6,7,8]
+        n=int(input("Enter the index"))
+        print(l[n])
+        return 1
+    except:
+        print("Here we have index error")
+        return 0
+
+    finally:
+        print("This line of code will be ececuted in every condtion")
+    
+result=fun()
+print("The value of result is",result)
+
