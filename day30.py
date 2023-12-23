bac8af4ba01b1bb8b669b64b12359170a86c83c6
Waris Hayat <warishayat69@gmail.com>
2023-12-23 15:21:27 +0000

    Module

---
+++ day30.py
@@ -0,0 +1,20 @@
+#in todays class we will explore the import module
+
+from math import *
+n=sqrt(250)*pi
+print()
+
+#as keyword its also called aliyas
+import math as m 
+r=m.cos(70)
+print(r)
+print(dir(m))
+
+
+
+result=m.sin(1)
+print(result)
+
+
+import random
+print(dir(random)) #to check the method of module.
