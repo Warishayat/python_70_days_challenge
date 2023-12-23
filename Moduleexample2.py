068df834dc21976a33a150e769fd11eca78dd428
Waris Hayat <warishayat69@gmail.com>
2023-12-23 16:02:51 +0000

    module exmp2

---
+++ Moduleexample2.py
@@ -0,0 +1,13 @@
+import math as mt 
+
+
+num=int(input("Enter the number1:"))
+num2=int(input("Enter the number2:"))
+
+result=num+num2
+
+#here i am going t aply diffrent method on result
+print("The cos of value:",mt.cos(result))
+print("The Acosh value:",mt.acosh(result))
+print("The LCM is",mt.lcm(result))
+print("The square root of the value is:",mt.sqrt(result))
