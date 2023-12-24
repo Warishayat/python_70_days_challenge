60ad2897c342c475133accdef9e551f8a785949a
Waris Hayat <warishayat69@gmail.com>
2023-12-24 12:38:00 +0000

    Day31_example

---
+++ Namemain.py
@@ -0,0 +1,10 @@
+# File: myscript.py
+
+def some_function():
+    print("Function in myscript.py")
+
+
+if __name__ == "__main__":
+    # This code will only run if myscript.py is executed directly
+    print("This script is being run directly.")
+    some_function()
