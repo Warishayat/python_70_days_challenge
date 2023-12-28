a7dfc9dcfc2c85a9d35388ddc878af8260dededb
Waris Hayat <warishayat69@gmail.com>
2023-12-28 18:14:48 +0000

    day36.py

---
+++ day36.py
@@ -0,0 +1,14 @@
+#today i will cover the seek(),tell() & truncat function....
+
+with open ("myfile.txt",'r') as f:
+    
+    result=f.seek(3) #move the 10 byte forward.
+
+    print(f.tell())    #it tell us about where we are
+    result1=f.read(10)
+    print(result1)
+
+
+with open ("Tetxt31.txt",'w') as f:
+    f.write("Hellow world")
+    f.truncate(3)
