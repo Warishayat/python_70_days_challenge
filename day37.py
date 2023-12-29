71d9ccfbfe827df2003a45c75c98c2f42f038654
Waris Hayat <warishayat69@gmail.com>
2023-12-29 14:08:46 +0000

    lambda function

---
+++ day37.py
@@ -0,0 +1,20 @@
+# now i will cover lamda function lambda is anonmous function
+#when we use lambda function where we nee one line
+x1= lambda x,y: x+y
+print(x1(12,14))
+
+#sm
+sum=lambda x,y: x+y
+print("The sum of two number is",sum(5,12))
+
+#average function
+avg=lambda x,y,z : (x+y+z)/2
+print("The average is=",avg(12,12,13))
+
+
+
+
+# Write a lambda function that takes two parameters (a and b) and returns their sum.
+
+sum=lambda x,y: x+y
+print(sum(5,7))
